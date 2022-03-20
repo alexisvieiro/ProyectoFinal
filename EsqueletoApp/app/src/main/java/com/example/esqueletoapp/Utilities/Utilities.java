4a9fe79496dfe3b593216774f661c5c9e609a08f
alexisvieiro <39537144+alexisvieiro@users.noreply.github.com>
2022-03-20 14:02:35 -0300

    Delete EsqueletoApp directory

--- EsqueletoApp/app/src/main/java/com/example/esqueletoapp/Utilities/Utilities.java
+++
@@ -1,8 +0,0 @@
-package com.example.esqueletoapp.Utilities;
-
-public class Utilities {
-    public static final String LOGIN_REQUEST_1 = "{\n   \"jsonrpc\": \"2.0\",\n   \"method\": " +
-            "\"user.login\",\n   \"params\": {\n       \"user\": \"" ;
-    public static final String LOGIN_REQUEST_2 = "\",\n       \"password\": \"";
-    public static final String LOGIN_REQUEST_3 = "\"\n   },\n   \"id\": 1,\n   \"auth\": null\n}\n";
-}
